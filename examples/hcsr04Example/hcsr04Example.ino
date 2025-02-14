aa01f04517b88923177f881fb0a6475ca4c48ae5
Hanz727 <oskar.rasienski@gmail.com>
2025-02-14 01:26:07 +0100

    fixed examples

--- examples/hcsr04Example/hcsr04Example.ino
+++ examples/hcsr04Example/hcsr04Example.ino
@@ -1,6 +1,6 @@
 #include "RangeFinder.h"
 
-const int sensors = {6,7,8,9,10,11}; // ECHO,TRIG pin pairs, two pins for each sensor
+const uint8_t sensors[] = {6,7,8,9,10,11}; // ECHO,TRIG pin pairs, two pins for each sensor
 
 RangeFinder rangeFinder;
 
@@ -8,7 +8,7 @@ void setup() {
     rangeFinder.init(sensors, 3, ULTRASONIC_HC_SR04); // The count is still 3, because we have 3 sensors.
 }
 
-void update() {
+void loop() {
     rangeFinder.update(); // updates the ranges for all sensors
 
     // Gets measured distance from sensor on pin 6 in cm
--- examples/pingExample/pingExample.ino
+++ examples/pingExample/pingExample.ino
@@ -1,6 +1,6 @@
 #include "RangeFinder.h"
 
-const int sensors = {6,7,8}; // PING pins, one pin for each sensor
+const uint8_t sensors[] = {6,7,8}; // PING pins, one pin for each sensor
 
 RangeFinder rangeFinder;
 
@@ -8,7 +8,7 @@ void setup() {
     rangeFinder.init(sensors, 3, ULTRASONIC_PING);
 }
 
-void update() {
+void loop() {
     rangeFinder.update(); // updates the ranges for all sensors
 
     // Gets measured distance from sensor on pin 6 in cm
